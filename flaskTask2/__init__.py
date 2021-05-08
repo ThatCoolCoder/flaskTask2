173c98e29a47d34a6176cb413261ff8fc9ef2316
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-05-08 17:05:11 +0930

    test

--- flaskTask2/__init__.py
+++ flaskTask2/__init__.py
@@ -1,9 +1,17 @@
+import shelve
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '<button onclick="alert(\'hi\')">Click me!</button>'
+    with open('main.html', 'r') as f:
+        return f.read()
+    return 'Unknown error'
+
+@app.route('/get', methods=['POST'])
+def get():
+    return [1, 2, 3, 4]
 
 if __name__ == '__main__':
     app.run()
