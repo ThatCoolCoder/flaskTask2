c2b1dfe26601b133a711071116d938ce5e3f6afe
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-05-08 17:58:00 +0930

    made chat

--- flaskTask2/__init__.py
+++ flaskTask2/__init__.py
@@ -1,17 +1,31 @@
-import shelve
-
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
-    with open('main.html', 'r') as f:
-        return f.read()
-    return 'Unknown error'
+messages = []
 
-@app.route('/get', methods=['POST'])
+@app.route('/', methods=['GET'])
+def homepage():
+    data = 'Error: Unknown error'
+    f = None
+    try:
+        f = app.open_resource('main.html', 'r')
+        data = f.read()
+    except Exception as e:
+        data = 'Error reading file: ' + str(e)
+    finally:
+        if f is not None:
+            f.close()
+        return data
+    
+
+@app.route('/get/', methods=['POST'])
 def get():
-    return [1, 2, 3, 4]
+    return jsonify(messages)
+
+@app.route('/send/', methods=['POST'])
+def send():
+    messages.append({'sender' : request.json['sender'], 'content' : request.json['content']})
+    return ''
 
 if __name__ == '__main__':
     app.run()
