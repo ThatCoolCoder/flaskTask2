bddaf8a4d24c664a4406448fc35b416a5a5f1288
That-Cool-Coder <62006241+That-Cool-Coder@users.noreply.github.com>
2021-05-09 16:33:33 +0930

    made it save to a file instead of to memory

--- flaskTask2/__init__.py
+++ flaskTask2/__init__.py
@@ -1,7 +1,26 @@
+import json
+import os.path
+
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-messages = []
+# The cwd starts off as / for some reason, so make the message file name absolute
+CONTAINING_DIR = os.path.dirname(os.path.realpath(__file__))
+MESSAGE_FILE_NAME = os.path.join(CONTAINING_DIR, 'messages.json')
+
+def load_messages():
+    if os.path.exists(MESSAGE_FILE_NAME):
+        f = open(MESSAGE_FILE_NAME, 'r')
+        messages = json.loads(f.read())
+        f.close()
+        return messages
+    else:
+        return []
+
+def save_messages(messages):
+    f = open(MESSAGE_FILE_NAME, 'w+')
+    f.write(json.dumps(messages))
+    f.close()
 
 @app.route('/', methods=['GET'])
 def homepage():
@@ -20,11 +39,13 @@ def homepage():
 
 @app.route('/get/', methods=['POST'])
 def get():
-    return jsonify(messages)
+    return jsonify(load_messages())
 
 @app.route('/send/', methods=['POST'])
 def send():
+    messages = load_messages()
     messages.append({'sender' : request.json['sender'], 'content' : request.json['content']})
+    save_messages(messages)
     return ''
 
 if __name__ == '__main__':
